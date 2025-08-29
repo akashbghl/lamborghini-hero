45319e722c4f1af6f9cb81fc852290083abb381f
Akash Baghel <akash27aug.2002@gmail.com>
2025-08-30 01:02:27 +0530

    mobile-layout-fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
         <Navbar />
 
         {/* Hero Section  */}
-        <div className=" md:px-28 md:py-16 p-10 min-h-[600px] text-white grid grid-cols-1 md:grid-cols-2 items-center">
+        <div className=" md:px-28 md:py-16 p-8 min-h-[600px] text-white grid grid-cols-1 md:grid-cols-2 items-center">
           {/* text section  */}
           <div className="max-md:flex flex-col items-center justify-center">
             <motion.div
@@ -28,9 +28,9 @@ const Hero = () => {
               transition={{ duration: 0.5, delay: 1.2 }}
               className="max-md:flex flex-col items-center justify-center"
             >
-              <h1 className="max-md:text-center text-4xl md:text-7xl font-bold">LAMBORGHINI <br /> <span className="text-transparent [-webkit-text-stroke:1px_white]  [-webkit-text-stroke-color:currentColor]">AVENDATOR</span></h1>
+              <h1 className="max-md:text-center text-5xl md:text-7xl font-bold">LAMBORGHINI <br /> <span className="text-transparent [-webkit-text-stroke:1px_white]  [-webkit-text-stroke-color:currentColor]">AVENDATOR</span></h1>
               <button
-                className="mt-4 md:mt-8 border border-white rounded-md  px-8 py-2 flex justify-center items-center gap-4 cursor-pointer">
+                className="mt-8 border border-white rounded-md  px-8 py-2 flex justify-center items-center gap-4 cursor-pointer">
                 Explore <FaArrowRight className="hover:translate-x-2.5 transition-all duration-200" /> </button>
             </motion.div>
 
@@ -39,14 +39,14 @@ const Hero = () => {
               initial={{ y: 500, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ duration: 0.5, delay: 1.6 }}
-              className="bg-black/30 p-6 max-w-[300px] md:max-w-[400px] mt-6 md:mt-10 shadow-2xl hover:shadow-[0px_0px_4px_gray] hover:transition-all">
+              className="bg-black/30 p-6 md:max-w-[400px] mt-10 shadow-2xl hover:shadow-[0px_0px_4px_gray] hover:transition-all">
               <h1 className="text-xl font-semibold mb-2">VEHICLE FEATURES</h1>
               <p className=" text-xs md:text-sm">The car features rear-hinged suicide doors. Yuki Terai is credited with the exterior design. The interior fits four adults, with the front passengers seated on a single bench seat. The dashboard is decorated in wood trim with a long digital screen as the instrument and infotainment panel</p>
             </motion.div>
           </div>
 
           {/* image section  */}
-          <div className="relative">
+          <div className="mt-10">
             <motion.div 
             initial={{y:80 ,rotateX:-10 , opacity:0}}
             animate={{y:0 ,rotateX:0 ,opacity:1}}
